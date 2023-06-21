5ff55471d418feaf4c6c1e3e409e0127b619b6c9
yara mohamed <75863913+yara857@users.noreply.github.com>
2023-06-22 02:03:12 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css';
+// import Cart from './pages/Cart';
+// import Product from './pages/Product';
+// import Login from './pages/Login';
+// import Register from './pages/Register';
+// import ProductList from './pages/ProductList';
+import Home from './pages/Home';
+function App() {
+  return (
+    <Home></Home>
+    // <ProductList></ProductList>
+    // <Product></Product>
+    // <Register></Register>
+    // <Login></Login>
+    // <Cart></Cart>
+  );
+}
+
+export default App;
